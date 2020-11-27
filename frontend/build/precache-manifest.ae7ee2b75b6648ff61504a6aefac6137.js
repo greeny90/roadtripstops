26671882a14e93dce58a7fe999bbc0ccdfaf36c9
Yvette Green <green.y@northeastern.edu>
2020-11-26 22:18:56 -0800

    frontend

---
+++ frontend/build/precache-manifest.ae7ee2b75b6648ff61504a6aefac6137.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdcc6b21b5202f244a3efb0c56f8c70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0279fe17408929cd9e",
+    "url": "/static/css/0.8aeb1022.chunk.css"
+  },
+  {
+    "revision": "52ffc495f7b6cf27abbb",
+    "url": "/static/css/4.6d344725.chunk.css"
+  },
+  {
+    "revision": "21a8f62bc95a03bbfa11",
+    "url": "/static/css/5.f3505a8f.chunk.css"
+  },
+  {
+    "revision": "df88ec9089598d13a20f",
+    "url": "/static/css/6.1b89bb29.chunk.css"
+  },
+  {
+    "revision": "0de841f2252ab3387a73",
+    "url": "/static/css/7.1569f0bd.chunk.css"
+  },
+  {
+    "revision": "208a8f8caf05243b0922",
+    "url": "/static/css/8.e1e90428.chunk.css"
+  },
+  {
+    "revision": "da5a8075d5772587160b",
+    "url": "/static/css/main.f76bf9f6.chunk.css"
+  },
+  {
+    "revision": "cf0279fe17408929cd9e",
+    "url": "/static/js/0.ab9f2686.chunk.js"
+  },
+  {
+    "revision": "8873a42a6c4ca1e3914f",
+    "url": "/static/js/3.573a9255.chunk.js"
+  },
+  {
+    "revision": "52ffc495f7b6cf27abbb",
+    "url": "/static/js/4.a75bf783.chunk.js"
+  },
+  {
+    "revision": "21a8f62bc95a03bbfa11",
+    "url": "/static/js/5.9347966c.chunk.js"
+  },
+  {
+    "revision": "df88ec9089598d13a20f",
+    "url": "/static/js/6.4ef638c5.chunk.js"
+  },
+  {
+    "revision": "0de841f2252ab3387a73",
+    "url": "/static/js/7.6346eecb.chunk.js"
+  },
+  {
+    "revision": "208a8f8caf05243b0922",
+    "url": "/static/js/8.995fcd8d.chunk.js"
+  },
+  {
+    "revision": "da5a8075d5772587160b",
+    "url": "/static/js/main.2a14f766.chunk.js"
+  },
+  {
+    "revision": "873fab10fbdf632cc92a",
+    "url": "/static/js/runtime-main.68e217b7.js"
+  }
+]);
---
+++ frontend/src/shared/components/Navigation/MainFooter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './MainFooter.css';
+
+const Footer = () => (
+  <div className="footer">
+    <h3>Explore and share your favorite road trip destinations!</h3>
+  </div>
+);
+
+export default Footer;
